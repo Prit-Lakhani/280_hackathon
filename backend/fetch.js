7adc23579a06d91bc91c8d6bdc0c4d1f61da3ebf
Varsha Suresh <varsha.suresh@sjsu.edu>
2022-11-19 14:54:39 -0800

    IndiaDatastore

---
+++ backend/fetch.js
@@ -0,0 +1,4 @@
+const datastore = require("./datastore");
+const years = require("./years.json");
+console.log(datastore.India.MacroEconomics.FDINetOutflows.length);
+console.log(years.Years.length);
